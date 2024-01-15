0edbb49ced3054c345c681b767e8dbddd0bc4557
hGauna <undrbug@gmail.com>
2024-01-15 12:41:57 -0300

    simplifique el metodo

--- back/src/renewSecretKey.js
+++ back/src/renewSecretKey.js
@@ -1,24 +1,29 @@
 import fs from 'fs';
 import path from 'path'
+import dotenv from 'dotenv';
 import crypto from 'crypto';
 
-// Establezco la ruta al archivo .env
-const envPath = path.join(__dirname, '.env');
-
-// Leo el contenido actual del archivo
-const currentEnvContent = fs.readFileSync(envPath, 'utf-8');
-
 export const generateAndRenewKey = () => {
-  // Genero una nueva clave
-  const newKey = crypto.randomBytes(32).toString('hex');
 
-  // Modificar el contenido según sea necesario
-const updatedEnvContent = currentEnvContent + `\SECRET_KEY=${newKey}`;
+    // Establezco la ruta al archivo .env
+    // const envPath = new URL('.env', import.meta.url).pathname;
 
-  // Guardo la nueva clave en el archivo .env
-  fs.writeFileSync(envPath, updatedEnvContent);
+    // Leo el contenido actual del archivo
+    // const currentEnvContent = fs.readFileSync(envPath, 'utf-8');
 
-  console.log('Nueva clave generada y actualizada en el archivo .env');
+    // Genero una nueva clave
+    const newKey = crypto.randomBytes(32).toString('hex');
+
+    process.env.SECRET_KEY = newKey;
+    console.log(process.env.SECRET_KEY);
+
+    // Modificar el contenido según sea necesario
+    // const updatedEnvContent = SECRET_KEY=`${newKey}`;
+
+    // Guardo la nueva clave en el archivo .env
+    // fs.writeFileSync(envPath, updatedEnvContent);
+
+    console.log('Nueva clave generada y actualizada en el archivo .env');
 }
 // Ejecuto la funcion segun algun criterio
 // En este caso cada 24 horas (86400000 milisegundos)
